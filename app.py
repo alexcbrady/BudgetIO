ce7b6d38918db62566f5f865a38650fdb4f68186
alexcbrady <114630867+alexcbrady@users.noreply.github.com>
2024-02-15 22:36:30 -0500

    Pandas dataframe -> xlsx

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import os
 import pymysql.cursors
 from tkinter import *
 from tkcalendar import Calendar, DateEntry
-
+import pandas as pd
+from datetime import date
+import openpyxl
+from openpyxl.chart import PieChart, Reference
+import time
 
 load_dotenv()
 
@@ -14,7 +18,7 @@ windowMain.geometry('600x400')
 
 
 #DATE INPUT
-dateText = Label(windowMain, text='Choose date of purchase:')
+dateText = Label(windowMain, text='Enter date of purchase:')
 dateText.grid(row=1, column=1)
 Bdate = DateEntry(windowMain,date_pattern='yyyy-mm-dd', foreground='white', background='white', width=19)
 Bdate.grid(column=2, row=1)
@@ -104,11 +108,18 @@ def newRangeWindow():
     categoryDrop.grid(row=3, column=2)
 
     #EXECUTE RANGE SELECTION BUTTON
-    executeRange = Button(rangeWindow, text='Execute', command= lambda:selectRangeSQL(lDate, rDate, lPriceVar.get(), rPriceVar.get(), categoryVarRange.get()))
+    executeRange = Button(rangeWindow, text='Execute', command= lambda:selectRangeSQL(lDate, rDate, lPriceVar.get(), rPriceVar.get(), categoryVarRange.get(), csvVar.get()))
     executeRange.grid(row=4, column=4)
 
-    executeWideOpen = Button(rangeWindow, text='Wide Open', command=lambda:SqlWideOpen())
+    executeWideOpen = Button(rangeWindow, text='Wide Open', command=lambda:SqlWideOpen(csvVar.get()))
     executeWideOpen.grid(row=5,column=4)
+
+    #CSV NAME
+    csvVar = StringVar()
+    csvLabel = Label(rangeWindow, text='*CSV file name:')
+    csvName = Entry(rangeWindow, textvariable=csvVar, background='white', foreground='black')
+    csvLabel.grid(column=1, row=4)
+    csvName.grid(column=2, row=4)
 
 
 #MENU BAR TKINTER
@@ -126,7 +137,7 @@ windowMain.config(menu=mainMenu)
 
 #--------SQL METHODS----------#
 
-#INSERTS PURCHASES INTO DB
+#INSERTS PURCHASES INTO DB -------METHOD--------
 def insertSQL(bdate : Calendar, amount , Where , Category ):
     cxn = pymysql.connect(host= os.getenv('HOST'), user= 'root', password=os.getenv('PASSWORD'), database='budget',cursorclass=pymysql.cursors.DictCursor )
     with cxn:
@@ -136,43 +147,79 @@ def insertSQL(bdate : Calendar, amount , Where , Category ):
             print(result)
         cxn.commit()
 
-#SEARCHES SQL DB VIA RANGE
-def selectRangeSQL(ldate : Calendar, rdate : Calendar, lprice, rprice, category):
+#SEARCHES SQL DB VIA RANGE -----METHOD--------
+def selectRangeSQL(ldate : Calendar, rdate : Calendar, lprice, rprice, category, xlName):
     cxn = pymysql.connect(host= os.getenv('HOST'), user= 'root', password=os.getenv('PASSWORD'), database='budget',cursorclass=pymysql.cursors.DictCursor )
     with cxn:
         with cxn.cursor() as cursor:
 
             #RANGE BY DATE, CATEGORY, AND PRICE
             if (ldate != '' and rdate != '') and lprice != '' and rprice !='' and category != '':
-                cursor.execute(f"SELECT Bdate, price, location, category FROM budget.expenses WHERE bdate BETWEEN '{ldate.get_date()}' AND '{rdate.get_date()}' AND category = '{category}' AND price BETWEEN {lprice} AND {rprice}")
-
+                query = f"SELECT Bdate, price, location, category FROM budget.expenses WHERE bdate BETWEEN '{ldate.get_date()}' AND '{rdate.get_date()}' AND category = '{category}' AND price BETWEEN {lprice} AND {rprice}"
+                df = pd.read_sql(query, cxn)
+                
             #RANGE BY DATE AND CATEGORY
             if (ldate != '' and rdate != '') and lprice == '' and rprice == '' and category != '':
-                cursor.execute(f"SELECT Bdate, price, location, category FROM budget.expenses WHERE bdate BETWEEN '{ldate.get_date()}' AND '{rdate.get_date()}' AND category = '{category}';") 
+                query = f"SELECT Bdate, price, location, category FROM budget.expenses WHERE bdate BETWEEN '{ldate.get_date()}' AND '{rdate.get_date()}' AND category = '{category}';"
+                df = pd.read_sql(query, cxn)
 
             #RANGE BY DATE AND PRICE
             if (ldate != '' and rdate != '') and lprice != '' and rprice != '' and category == '':
-                cursor.execute(f"SELECT Bdate, price, location, category FROM budget.expenses WHERE bdate BETWEEN '{ldate.get_date()}' AND '{rdate.get_date()}' AND price BETWEEN {lprice} AND {rprice};") 
+                query = f"SELECT Bdate, price, location, category FROM budget.expenses WHERE bdate BETWEEN '{ldate.get_date()}' AND '{rdate.get_date()}' AND price BETWEEN {lprice} AND {rprice};"
+                df = pd.read_sql(query, cxn)
 
             #RANGE ONLY BY DATE
             if (ldate != '' and rdate != '') and lprice == '' and rprice == '' and category == '':
-                cursor.execute(f"SELECT Bdate, price, location, category FROM budget.expenses WHERE bdate BETWEEN '{ldate.get_date()}' AND '{rdate.get_date()}';") 
-
-            result = cursor.fetchone()
-            print(result)
+                query = f"SELECT Bdate, price, location, category FROM budget.expenses WHERE bdate BETWEEN '{ldate.get_date()}' AND '{rdate.get_date()}';"
+                df = pd.read_sql(query, cxn)
+    
         cxn.commit()
+    df.to_excel(f"{os.getenv('BUDGETIO_OUTPUT_PATH')}{xlName} {ldate.get_date()} {rdate.get_date()}.xlsx")
 
-#FOR RANGE PAGE - WIDE OPEN
-def SqlWideOpen():
+    #CREATING PIE CHART IN EXCEL
+    workbook = openpyxl.load_workbook(f"{os.getenv('BUDGETIO_OUTPUT_PATH')}{xlName} {ldate.get_date()} {rdate.get_date()}.xlsx")
+    sheet = workbook.active
+    chart = PieChart()
+    labels = Reference(sheet, min_col=5, max_col=5, min_row=2, max_row=len(df.index)+1)
+    data = Reference(sheet, min_col=3, max_col=3, min_row=2, max_row=len(df.index)+1)
+    chart.add_data(data, titles_from_data=True)
+    chart.set_categories(labels)
+    chart.title = 'Categorical Spending'
+    sheet.add_chart(chart, 'G2')
+
+    #FORMATTING DATE COLUMN
+    dateCol = sheet.column_dimensions['B']
+    dateCol.number_format = 'YYYY MM DD'
+
+    workbook.save(f"{os.getenv('BUDGETIO_OUTPUT_PATH')}{xlName} {ldate.get_date()} {rdate.get_date()}.xlsx")
+
+#FOR RANGE PAGE - WIDE OPEN ----- METHOD ----------
+def SqlWideOpen(xlName):
     cxn = pymysql.connect(host= os.getenv('HOST'), user= 'root', password=os.getenv('PASSWORD'), database='budget',cursorclass=pymysql.cursors.DictCursor )
     with cxn:
         with cxn.cursor() as cursor:
-            cursor.execute("SELECT * FROM Budget.expenses;")
-            result = cursor.fetchone()
-            print(result)
+            query = "SELECT * FROM Budget.expenses;"
+            df = pd.read_sql(query, cxn)
         cxn.commit()
+    df.to_excel(f"{os.getenv('BUDGETIO_OUTPUT_PATH')}{xlName} {date.today()}.xlsx")
+    
+    
+    #CREATING PIE CHART IN EXCEL
+    workbook = openpyxl.load_workbook(f"{os.getenv('BUDGETIO_OUTPUT_PATH')}{xlName} {date.today()}.xlsx")
+    sheet = workbook.active
+    chart = PieChart()
+    labels = Reference(sheet, min_col=5, max_col=5, min_row=2, max_row=len(df.index)+1)
+    data = Reference(sheet, min_col=3, max_col=3, min_row=2, max_row=len(df.index)+1)
+    chart.add_data(data, titles_from_data=True)
+    chart.set_categories(labels)
+    chart.title = 'Categorical Spending'
+    sheet.add_chart(chart, 'G2')
+    
+    #FORMATTING DATE COLUMN
+    dateCol = sheet.column_dimensions['B']
+    dateCol.number_format = 'YYYY MM DD'
 
- 
+    workbook.save(f"{os.getenv('BUDGETIO_OUTPUT_PATH')}{xlName} {date.today()}.xlsx")
 
 
 
